71f54d3afb7fb4f435931b7257141233548ac81f
lxh <liliangyang520@1633.com>
2014-04-29 10:59:17 +0800

    add jfinaldemo
    add project

---
+++ src/main/java/com/demo/blog/BlogValidator.java
@@ -0,0 +1,25 @@
+package com.demo.blog;
+
+import com.jfinal.core.Controller;
+import com.jfinal.validate.Validator;
+
+/**
+ * BlogValidator.
+ */
+public class BlogValidator extends Validator {
+	
+	protected void validate(Controller controller) {
+		validateRequiredString("blog.title", "titleMsg", "!!!请输入Blog标题!!!!");
+		validateRequiredString("blog.content", "contentMsg", "000请输入Blog内容2222!");
+	}
+	
+	protected void handleError(Controller controller) {
+		controller.keepModel(Blog.class);
+		
+		String actionKey = getActionKey();
+		if (actionKey.equals("/blog/save"))
+			controller.render("add.html");
+		else if (actionKey.equals("/blog/update"))
+			controller.render("edit.html");
+	}
+}
